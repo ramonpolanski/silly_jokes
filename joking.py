680988c56ced65fea1184f569efd1ad5e63cd2c9
RogerIsk <rogeriskrenov@gmail.com>
2024-03-15 15:26:15 +0100

    Rogers pythoncommit

---
+++ joking.py
@@ -0,0 +1,5 @@
+print('My name is Rudolf''\n')
+jokes = ['Why don’t scientists trust atoms?', 'Because they make up everything!', 'Did you hear about the mathematician who’s afraid of negative numbers?', 'He will stop at nothing to avoid them.']
+
+for joke in jokes:
+    print(joke)
