288e252b534c4e053a8ee95e7650b9a33b6713e1
RogerIsk <rogeriskrenov@gmail.com>
2024-03-15 15:29:31 +0100

    Added new secret jokes as comments

--- joking.py
+++ joking.py
@@ -3,3 +3,14 @@ jokes = ['Why don’t scientists trust atoms?', 'Because they make up everything
 
 for joke in jokes:
     print(joke)
+
+    #    Did you hear about the restaurant on the moon? Great food, no atmosphere.
+    # Why don't scientists trust atoms? Because they make up everything!
+    # I'm reading a book on anti-gravity. It's impossible to put down!
+    # Why don't skeletons fight each other? They don't have the guts.
+    # What do you call fake spaghetti? An impasta!
+    # I told my wife she was drawing her eyebrows too high. She looked surprised.
+    # Why don't eggs tell jokes? Because they might crack up!
+    # I'm on a seafood diet. I see food and I eat it.
+    # How does a penguin build its house? Igloos it together!
+    # I used to play piano by ear, but now I use my hands.
